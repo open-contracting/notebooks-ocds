3251c01b9a50262b3259922e20030fe84b03cad0
Duncan Dewhurst <duncan.dewhurst@opendataservices.coop>
2021-09-16 17:00:54 +1200

    fix flake8 errors

--- manage.py
+++ manage.py
@@ -10,25 +10,29 @@ import click
 from nbformat import reads
 from nbformat import write as write_notebook
 from nbmerge import merge_notebooks
-from oauth2client.service_account import ServiceAccountCredentials
 
 # A dict of notebooks and their components, identified by filename, excluding '.ipynb'
-NOTEBOOKS = {'publisher_analysis_template': ['setup_environment',
-                                             'choose_data',
-                                             'check_for_errors',
-                                             'check_scope'],
-            'meta_analysis_template': ['setup_environment'],
-            'structure_and_format_feedback_template': ['setup_environment',
-                                                       'choose_data',
-                                                       'check_for_errors',
-                                                       'check_scope',
-                                                       'check_structure_and_format'],
-            'data_quality_feedback_template': ['setup_environment',
-                                               'choose_data',
-                                               'check_for_errors',
-                                               'check_scope',
-                                               'check_structure_and_format',
-                                               'check_data_quality']}
+NOTEBOOKS = {
+    'publisher_analysis_template': [
+        'setup_environment',
+        'choose_data',
+        'check_for_errors',
+        'check_scope'],
+    'meta_analysis_template': ['setup_environment'],
+    'structure_and_format_feedback_template': [
+        'setup_environment',
+        'choose_data',
+        'check_for_errors',
+        'check_scope',
+        'check_structure_and_format'],
+    'data_quality_feedback_template': [
+        'setup_environment',
+        'choose_data',
+        'check_for_errors',
+        'check_scope',
+        'check_structure_and_format',
+        'check_data_quality']}
+
 
 def yield_notebooks():
     directory = Path(__file__).resolve().parent
@@ -58,7 +62,7 @@ def yield_cells(notebook):
                                    universal_newlines=True)
 
         yield source, cell, sql, pg_format.stdout
-        
+
 
 @click.group()
 def cli():
@@ -70,7 +74,7 @@ def pre_commit():
     """
     Format SQL cells in Jupyter Notebooks using pg_format and merge components to build notebooks.
     """
-    
+
     # Format SQL cells
     for filename, filepath, notebook in yield_notebooks():
         for source, cell, sql, sql_formatted in yield_cells(notebook):
@@ -82,19 +86,22 @@ def pre_commit():
 
     # Merge notebooks
     for slug, components in NOTEBOOKS.items():
-        
-        notebook = merge_notebooks(Path(__file__).resolve().parent,
-                                   [f'{component}.ipynb' for component in components],
-                                   False,
-                                   None)
-        
+
+        notebook = merge_notebooks(
+                    Path(__file__).resolve().parent,
+                    [f'{component}.ipynb' for component in components],
+                    False,
+                    None)
+
         with open(f'{slug}.ipynb', 'w', encoding='utf8') as f:
             write_notebook(notebook, f)
+
 
 @cli.command()
 def check():
     """
-    Check that notebooks and components are in sync. Check that SQL cells in Jupyter Notebooks are formatted using pg_format.
+    Check that notebooks and components are in sync.
+    Check that SQL cells in Jupyter Notebooks are formatted using pg_format.
     """
     warnings = False
     cwd = Path(__file__).resolve().parent
@@ -102,14 +109,18 @@ def check():
     for filename, filepath, notebook in yield_notebooks():
         slug = filename.split('.')[0]
         if slug in NOTEBOOKS:
-            merged_notebook = merge_notebooks(cwd,
-                                              [f'{component}.ipynb' for component in NOTEBOOKS[slug]],
-                                              False,
-                                              None)
+            merged_notebook = merge_notebooks(
+                                cwd,
+                                [f'{component}.ipynb' for component in NOTEBOOKS[slug]],
+                                False,
+                                None)
             if reads(json.dumps(notebook), as_version=4) != merged_notebook:
-                warnings=True
-                logging.warning(f'Mismatch between notebook {filename} and its components. If you edited a component, run `./manage.py pre-commit` to update the notebook. If you edited the notebook, copy your changes to the relevant components.')
-                
+                warnings = True
+                logging.warning(
+                    f'Mismatch between notebook {filename} and its components. If you edited a component, run '
+                    './manage.py pre-commit to update the notebook. If you edited the notebook, '
+                    'copy your changes to the relevant components.')
+
         for source, cell, sql, sql_formatted in yield_cells(notebook):
             if sql.strip() != sql_formatted.strip():
                 warnings = True
